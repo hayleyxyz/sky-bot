fcde0f47266f7e2ddaeeb934b2279d96705c1f7a
Oscar Berry <oscar@madokami.com>
2016-07-16 01:48:26 +0100

    Initial

---
+++ sky-bot/Program.cs
@@ -0,0 +1,12 @@
+﻿ i using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sky_bot {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
